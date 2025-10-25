5a3adcd3ad41552566d10065396fbaf13db5aba3
Tabrez Ahmed <225601255+tabrezahmed51@users.noreply.github.com>
2025-10-25 22:36:54 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,8 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TEXTBELT_URL = "https://textbelt.com/text"
+TEXTBELT_KEY = os.getenv("TEXTBELT_KEY", "textbelt")  # Default to demo key
